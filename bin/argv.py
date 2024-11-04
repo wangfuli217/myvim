622e7c0145a1be2894f6d639ddd32990bb932aba
wangfuli217 <wangfl217@126.com>
2024-11-04 08:53:08 +0800

    frg with option

---
+++ bin/argv.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python2
+from __future__ import print_function
+
+import sys
+
+print(sys.argv[1:])
---
+++ bin/stdout_stderr.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+from __future__ import print_function
+"""
+stdout_stderr.py
+
+Tool for testing redirects.
+"""
+
+import sys
+
+
+def main(argv):
+    try:
+        stdout = argv[1]
+    except IndexError:
+        stdout = 'STDOUT'
+
+    try:
+        stderr = argv[2]
+    except IndexError:
+        stderr = 'STDERR'
+
+    try:
+        status = int(argv[3])
+    except IndexError:
+        status = 0
+
+    print(stdout)
+    print(stderr, file=sys.stderr)
+    return status
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
